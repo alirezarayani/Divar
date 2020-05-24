dba94036aeaddcd270d2c44fc0c3767ba107cb37
alireza.rayani <alireza.rayani@gmail.com>
2020-05-24 22:57:56 +0430

    init project

---
+++ server/src/main/java/io/rayani/divar/DivarApplication.java
@@ -0,0 +1,13 @@
+package io.rayani.divar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DivarApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/io/rayani/divar/DivarApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rayani.divar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
